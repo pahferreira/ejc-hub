e63ec0aa452613844ac3a2a94a2a454f77a88b27
Pah Ferreira <pah.dsf@gmail.com>
2025-06-02 17:45:42 -0300

    feat: linting and formatting (#11)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,22 +1,23 @@
-import fastify from "fastify";
-import { env } from "./config/env.js"; // Assuming you have an env.js file for configuration
+import fastify from 'fastify'
+import { env } from './config/env.js' // Assuming you have an env.js file for configuration
 
 const server = fastify({
   logger: true,
-});
+})
 
-server.get("/ping", async (request, reply) => {
-  return { message: "pong" };
-});
+server.get('/ping', () => {
+  return { message: 'pong' }
+})
 
 async function startServer() {
   try {
-    await server.listen({ port: env.PORT });
-    server.log.info(`Server is running on http://localhost:${env.PORT}`);
+    await server.listen({ port: env.PORT })
+    server.log.info(`Server is running on http://localhost:${env.PORT}`)
   } catch (err) {
-    server.log.error(err);
-    process.exit(1);
+    server.log.error(err)
   }
 }
 
-startServer();
+startServer().catch(() => {
+  process.exit(1)
+})
