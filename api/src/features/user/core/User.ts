6956a3761b5231abaeb6e584fc6b2c0910d310e7
Pah Ferreira <pah.dsf@gmail.com>
2025-07-09 15:00:02 -0300

    feat(users): update user profile (#73)

--- api/src/features/user/core/User.ts
+++ api/src/features/user/core/User.ts
@@ -8,8 +8,22 @@ const createUserInputSchema = z.object({
   email: z.email('email is required'),
   picture: z.string(),
 })
-
 type CreateUserInputSchema = z.infer<typeof createUserInputSchema>
+
+const updateUserInputSchema = z.object({
+  phone: z.string().nullable().optional(),
+  nickname: z.string().nullable().optional(),
+  dateOfBirth: z.string().nullable().optional(),
+  hasMusicSkills: z.boolean().optional(),
+  hasActingSkills: z.boolean().optional(),
+  hasDancingSkills: z.boolean().optional(),
+  hasSingingSkills: z.boolean().optional(),
+  hasManualSkills: z.boolean().optional(),
+  hasCookingSkills: z.boolean().optional(),
+  hasCommunicationSkills: z.boolean().optional(),
+})
+
+type UpdateUserInputSchema = z.infer<typeof updateUserInputSchema>
 
 export class User {
   #userRepository: UserRepository
@@ -38,5 +52,26 @@ export class User {
     })
 
     return createdUser
+  }
+
+  async updateUser(authId: string, input: UpdateUserInputSchema) {
+    const validateInput = updateUserInputSchema.safeParse(input)
+
+    if (!validateInput.success) {
+      throw new AppError(validateInput.error.message)
+    }
+
+    const userToUpdate = await this.#userRepository.getUser(authId)
+
+    if (!userToUpdate) {
+      throw new AppError('user not found')
+    }
+
+    const updatedUser = await this.#userRepository.updateUser(userToUpdate.id, {
+      ...input,
+      dateOfBirth: input.dateOfBirth ? new Date(input.dateOfBirth) : undefined,
+    })
+
+    return updatedUser
   }
 }
--- api/src/features/user/http/user.routes.ts
+++ api/src/features/user/http/user.routes.ts
@@ -3,12 +3,26 @@ import { decodeJwt } from 'jose'
 import { HttpStatus } from '../../../shared/http-statuses.ts'
 import { userApp } from '../application/user.ts'
 import { fastifyRequireAuth } from '../../../shared/fastify-require-auth.ts'
+import z from 'zod/v4'
 
 type UserInfo = {
   email: string
   name: string
   picture: string
 }
+
+const updateUserInputSchema = z.object({
+  phone: z.string().nullable().optional(),
+  nickname: z.string().nullable().optional(),
+  dateOfBirth: z.string().nullable().optional(),
+  hasMusicSkills: z.boolean().optional(),
+  hasActingSkills: z.boolean().optional(),
+  hasDancingSkills: z.boolean().optional(),
+  hasSingingSkills: z.boolean().optional(),
+  hasManualSkills: z.boolean().optional(),
+  hasCookingSkills: z.boolean().optional(),
+  hasCommunicationSkills: z.boolean().optional(),
+})
 
 export function userRoutes(server: FastifyServerInstance) {
   return () => {
@@ -21,6 +35,21 @@ export function userRoutes(server: FastifyServerInstance) {
         if (token) {
           const { authId, email, name, picture } = extractUserInformationFromToken(token)
           const user = await userApp.createUser({ authId, name, email, picture })
+
+          return reply.code(HttpStatus.Ok).send({ user })
+        }
+      }
+    )
+
+    server.patch(
+      '/users/profile',
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      { schema: { body: updateUserInputSchema }, preHandler: fastifyRequireAuth(server) },
+      async (request, reply) => {
+        const token = request.getToken()
+        if (token) {
+          const { authId } = extractUserInformationFromToken(token)
+          const user = await userApp.updateUser(authId, request.body)
 
           return reply.code(HttpStatus.Ok).send({ user })
         }
