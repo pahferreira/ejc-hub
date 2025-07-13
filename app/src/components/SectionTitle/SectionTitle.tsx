1efc8813ce2458fd218b730404bdc8ade36cf6fe
ManewJ <manoeljustino@gmail.com>
2025-07-13 16:35:29 -0300

    refactor: css adaptations

--- app/src/components/SectionTitle/SectionTitle.tsx
+++ app/src/components/SectionTitle/SectionTitle.tsx
@@ -1,19 +1,15 @@
-import "./SectionTitle.css";
+import './SectionTitle.css'
 
 type SectionTitleProps = {
-    title: string;
-    description?: string;
-    };
+  title: string
+  description?: string
+}
 
-export function SectionTitle(props : SectionTitleProps) {
+export function SectionTitle(props: SectionTitleProps) {
   return (
     <div>
       <h1 className="section-title">{props.title}</h1>
       <p className="section-description">{props.description}</p>
     </div>
-  );
+  )
 }
-
-
-
-
