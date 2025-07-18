83867df96c65a47e25f13b4816fea23746893501
Manoel Justino <161733275+ManewJ@users.noreply.github.com>
2025-07-17 21:02:52 -0300

    refactor: section title with tailwind (#102)

--- app/src/components/SectionTitle/SectionTitle.tsx
+++ app/src/components/SectionTitle/SectionTitle.tsx
@@ -8,8 +8,8 @@ type SectionTitleProps = {
 export function SectionTitle(props: SectionTitleProps) {
   return (
     <div>
-      <h1 className="section-title">{props.title}</h1>
-      <p className="section-description">{props.description}</p>
+      <h1 className="text-2xl font-bold m-0">{props.title}</h1>
+      <p className="text-base not-italic font-normal my-2">{props.description}</p>
     </div>
   )
 }
