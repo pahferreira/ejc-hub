5e574c381363fd165be8f228d2947a41d6dd21c3
Pah Ferreira <pah.dsf@gmail.com>
2026-01-23 16:52:02 -0300

    refactor(app): navbar refactor + storybook clean up (#134)

--- app/src/components/navbar/Navbar.tsx
+++ app/src/components/navbar/Navbar.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
-
-import './styles/navbar.css'
 import { Navlink } from './Navlink'
 
-export const Navbar = () => (
-  <header>
-    <div className='navbar'>
-      <div className='navbar-content'>
-        <div className='navbar-brand'>
-          <img src='/church.svg' alt='Igreja' className='navbar-logo' />
-          <h1>EJC Rosário</h1>
-        </div>
-        <div className='navbar-routes'>
-          <Navlink to='/registration' name='Inscrição' />
-          <Navlink to='/dashboard' name='Dashboard' />
-          <Navlink to='/teams' name='Equipes' />
-        </div>
+type NavbarProps = {
+  title?: string
+  imageSrc?: string
+  navItems?: Array<{ name: string; to: string }>
+}
+
+export const Navbar = (props: NavbarProps) => (
+  <header className="border-b border-slate-300/10">
+    <nav className="flex items-center px-14 py-4">
+      <div className="flex items-center">
+        {props.imageSrc && (
+          <figure>
+            <img src={props.imageSrc || '/church.svg'} alt="Igreja" className="mr-3" />
+          </figure>
+        )}
+        {props.title && <h1 className="m-0 text-xl font-bold leading-none">{props.title}</h1>}
       </div>
-    </div>
+      <ul className="ml-[54px] flex gap-8 list-none">
+        {props.navItems?.map((item) => (
+          <li key={item.to}>
+            <Navlink to={item.to} name={item.name} />
+          </li>
+        ))}
+      </ul>
+    </nav>
   </header>
 )
