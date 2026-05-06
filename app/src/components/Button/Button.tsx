3a34e7a3e968259f0afe794a3e093058f907141e
Pah Ferreira <pahferreira.dev@gmail.com>
2026-05-06 20:33:51 -0300

    refactor(app): button ui refactoring

--- app/src/components/Button/Button.tsx
+++ app/src/components/Button/Button.tsx
@@ -10,24 +10,22 @@ type ButtonProps = {
 
 const foregroundVariant = {
   primary: 'bg-primary text-white',
-  secondary: 'bg-secondary text-dark-brown border-secondary',
+  secondary: 'bg-secondary text-dark-brown',
 }
 
 export function Button(props: ButtonProps) {
   const variant = props.variant || 'primary'
+
   return (
-    <div className="relative w-fit pb-1">
-      <div className="absolute inset-0 rounded-lg bg-dark-brown" />
-      <button
-        onClick={props.onClick}
-        disabled={props.disabled}
-        className={clsx(
-          'relative w-fit cursor-pointer rounded-lg border border-dark-brown py-2 px-4 text-center font-bold hover:-translate-y-1 disabled:translate-y-0 disabled:cursor-not-allowed disabled:opacity-50',
-          foregroundVariant[variant]
-        )}
-      >
-        {props.children}
-      </button>
-    </div>
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={clsx(
+        foregroundVariant[variant],
+        'px-3 py-1.5 text-xs font-medium gap-2 border-2 border-dark-brown rounded-lg button-shadow hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50'
+      )}
+    >
+      {props.children}
+    </button>
   )
 }
